b38084031dac98a94c2efeab94708f2bb38a3ef6
Bernd Vogt <Bernd.Vogt@bosch-si.com>
2014-09-29 14:40:11 +0200

    #2: Apache License Version 2.0

--- src/main/java/org/sourcepit/tpmp/change/ChecksumTargetPlatformConfigurationChangeDiscoverer.java
+++ src/main/java/org/sourcepit/tpmp/change/ChecksumTargetPlatformConfigurationChangeDiscoverer.java
@@ -1,7 +1,17 @@
-/**
- * Copyright (c) 2012 Sourcepit.org contributors and others. All rights reserved. This program and the accompanying
- * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+/*
+ * Copyright 2014 Bernd Vogt and others.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.sourcepit.tpmp.change;
@@ -216,8 +226,8 @@ public class ChecksumTargetPlatformConfigurationChangeDiscoverer implements Targ
       return calculateHash(sha1, buffIn(byteIn(bytes)), encoding);
    }
 
-   private static String calculateHash(final MessageDigest algorithm,
-      final IOHandle<? extends InputStream> ioResource, final String encoding)
+   private static String calculateHash(final MessageDigest algorithm, final IOHandle<? extends InputStream> ioResource,
+      final String encoding)
    {
       Reader reader = null;
       InputStream inputStream = null;
